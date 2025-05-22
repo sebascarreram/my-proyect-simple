39ac9e2de6fe27265b7135e4ad00ce1a02bcd14a
sebastian <sebascarreram@hotmail.com>
2025-05-22 16:37:16 -0500

    changed name

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Sebastian
+            Paola
           </li>
         </ol>
 
